a5d6b51a760f7c3a41705ce01518550b27bf2a25
顾兆远 <2469754120@qq.com>
2020-11-09 22:14:46 +0800

    v4

--- assets/script/Bird.js
+++ assets/script/Bird.js
@@ -1,109 +1,212 @@
-const State = cc.Enum({
-    /** 游戏开始前的准备状态 */
-    Ready: -1,
-    /** 小鸟上升中 */
-    Rise: -1,
-    /** 小鸟自由落体中 */
-    FreeFall: -1,
-    /** 小鸟碰撞到管道坠落中 */
-    Drop: -1,
-    /** 小鸟已坠落到地面静止 */
-    Dead: -1,
-});
+const State =cc.Enum({
+    //游戏开始前的准备
+    Ready:-1,
+    //小鸟上升中
+    Rise:-1,
+    //小鸟自由落体
+    FreeFall:-1,
+    //碰撞管道掉落
+    Drop:-1,
+    //坠落地面静止
+    Dead:-1,
+})
 
 cc.Class({
-    statics: {
-        State: State
-    },
-
     extends: cc.Component,
-
+    statics:{
+        State:State
+    },
+    
     properties: {
-        /** 上抛初速度，单位：像素/秒 */
-        initRiseSpeed: 800,
-        /** 重力加速度，单位：像素/秒的平方 */
-        gravity: 1000,
-        /** 小鸟的状态 */
-        state: {
-            default: State.Ready,
-            type: State,
+        initRiseSpeed:800,
+        gravity:1000,
+        ground:{
+            default:null,
+            type:cc.Node
         },
-        /** 地面节点 */
-        ground: {
-            default: null,
-            type: cc.Node
+        state:{
+            default:State.Ready,
+            type:State
         },
+
+        //小鸟向上飞的声音
+        riseAudio:{
+            default:null,
+            url:cc.AudioClip
+        },
+        //小鸟碰撞到水管后开始坠落的声音
+        dropAudio:{
+            default:null,
+            url:cc.AudioClip
+        },
+        //小鸟发生碰撞的声音
+        hitAudio:{
+            default:null,
+            url:cc.AudioClip
+        },
+      
     },
 
-    init(game) {
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+    init(game){
         this.game = game;
         this.state = State.Ready;
-        this.currentSpeed = 0;//初始化当前的速度
-        this.anim = this.getComponent(cc.Animation);//获取动画组件
+        this.currentSpeed = 0;  //初始化当前的速度
+        this.anim = this.getComponent(cc.Animation);  //获取动画组件
     },
-    update(dt) {
-        if (this.state === State.Ready || this.state === State.Dead) {//如果是住呢比或者死亡状态则停止计算
+    update (dt) {
+        //如果是住呢比或者死亡状态则停止计算
+        if(this.state === State.Ready || this.state === State.Dead){
             return;
         }
         this._updatePosition(dt);
         this._updateState(dt);
+        this.detectCollision();
+        this.fixBirdFinalPosition();
     },
-    _updatePosition(dt) {
+    
+
+    //在获得状态的时候，我们要定义计算位置的函数，并保持更新。
+    _updatePosition(dt){
         var flying = this.state === State.Rise
-            || this.state === State.FreeFall
-            || this.state === State.Drop;//判断小鸟是否处于飞行状态
-        if (flying) {//如果处于飞行状态
-            var h = cc.director.getVisibleSize().height / 2;
-            if (this.node.y >= h) {//当小鸟的高度超过上边的时候，让小鸟的位置和速度降下来
-                this.node.y = h - 1;
-                this.currentSpeed = -1;
-            } else {
-                this.currentSpeed -= dt * this.gravity;//根据预先设置好的重力计算当前的速度
-                this.node.y += dt * this.currentSpeed;//根据计算出来的y轴速度，计算出当前小鸟的位置
-            }
-        }
+         || this.state === State.FreeFall
+         //判断小鸟是否处于飞行状态
+         || this.state === State.Drop;
+         //如果处于飞行状态
+         if(flying){
+             var h = cc.director.getVisibleSize().height / 2;
+             //当小鸟的高度超过上边的时候，让小鸟的位置个速度降下来
+             if(this.node.y >= h){
+                 this.node.y = h - 1;
+                 this.currentSpeed = -1;
+             }else{
+                 //根据预先设置好的重力计算当前的速度
+                 this.currentSpeed -= dt * this.gravity;
+                 //根据计算出来的y轴速度，计算当前的小鸟的位置
+                 this.node.y += dt * this.currentSpeed;
+             }
+         }
     },
 
-    _updateState(dt) {
-        switch (this.state) {
-            case State.Rise://当当前状态是飞起的时候，如果速度小于零了，则转换为自由下落状态
-                if (this.currentSpeed < 0) {
+    //计算状态
+    _updateState(dt){
+        switch(this.state){
+            //当当前的状态是飞起的时候，如果速度小于0的了，则转换为自由下落状态
+            case State.Rise:
+                if(this.currentSpeed <0){
                     this.state = State.FreeFall;
-                    this._runFallAction();
+                    this.runFallAction();
                 }
                 break;
         }
     },
-    //起飞函数
-    rise() {
+
+    //起飞
+    rise(){
+        //修改当前的状态为Rise
         this.state = State.Rise;
+
+        this.runRiseAction();
+        //重置速度
         this.currentSpeed = this.initRiseSpeed;
-        this._runRiseAction();
+
+        cc.audioEngine.playEffect(this.riseAudio);
     },
-    //开始起飞
-    startFly() {
+
+    //开始起飞,同时停止birdFlapping的动画，有冲突
+    startFly(){
+        this.getNextPipe();
         this.anim.stop("birdFlapping");
         this.rise();
     },
-    //向上飞的角度偏移
-    _runRiseAction() {
+    //设置鸟的偏移  起飞
+    runRiseAction(){
         this.node.stopAllActions();
-        let jumpAction = cc.rotateTo(0.3, -30).easing(cc.easeCubicActionOut());
+        let jumpAction = cc.rotateTo(0.3,-30).easing(cc.easeCubicActionOut());
         this.node.runAction(jumpAction);
     },
-
-    //下落的角度偏移
-    _runFallAction(duration = 0.6) {
+    //下落的角度偏移  下落
+    runFallAction(duration = 0.6){
         this.node.stopAllActions();
-        let dropAction = cc.rotateTo(duration, 90).easing(cc.easeCubicActionIn());
+        let dropAction = cc.rotateTo(duration,90).easing(cc.easeCubicActionIn());
         this.node.runAction(dropAction);
+
+        
+    },
+    //当小鸟碰撞到管道的时候坠落的角度偏移
+    runDropAction(){
+        if(this.currentSpeed>=0){
+            this.currentSpeed=0;
+        }
+        this.runFallAction(0.4);
+    },
+    getNextPipe(){
+        this.nextPipe = this.game.pipeManager.getNext();
     },
 
-    //当小鸟碰撞到管道的时候坠落的角度偏移
-    _runDropAction() {
-        if (this.currentSpeed > 0) {
-            this.currentSpeed = 0;
+    detectCollision(){
+        if(!this.nextPipe){
+            this.getNextPipe();
+            return;
         }
-        this._runFallAction(0.4);
-    }
+        if(this.state=== State.Ready || this.state === State.Dead || this.state === State.Drop){
+            return;
+        }
+
+        let collideWithPipe = false;
+        //检测小鸟与上方管子的碰撞
+        if(this.detectCollisionWithBird(this.nextPipe.topPipe)){
+            collideWithPipe = true;
+        }
+        //检测小鸟与下方管子的碰撞
+        if(this.detectCollisionWithBird(this.nextPipe.bottomPipe)){
+            collideWithPipe = true;
+        }
+        //检测小鸟与地面的碰撞
+        let collideWithGround = false;
+        if(this.detectCollisionWithBird(this.ground)){
+            collideWithGround = true;
+        }
+
+        //碰撞结果
+        if(collideWithPipe || collideWithGround){
+            cc.audioEngine.playEffect(this.hitAudio);
+            if(collideWithGround){   //与地面碰撞
+                this.state = State.Dead;
+            }else{    //与水管碰撞
+                this.state = State.Drop;
+                this.runDropAction();
+                this.scheduleOnce(()=>{
+                    cc.audioEngine.playEffect(this.dropAudio)
+                },0.3);
+            }
+
+            this.anim.stop();
+            this.game.gameOver();
+        }else{
+            let birdLeft = this.node.x;
+            let pipeRight = this.nextPipe.node.x+this.nextPipe.topPipe.width;
+            let crossPipe = birdLeft > pipeRight;
+            if(crossPipe){
+                this.game.gainScore();
+                this.getNextPipe();
+            }
+        }
+    },
+
+    detectCollisionWithBird(otherNode){
+        return cc.rectIntersectsRect(this.node.getBoundingBoxToWorld(),otherNode.getBoundingBoxToWorld());
+    },
+    //修正最后落地位置
+    fixBirdFinalPosition(){
+        if(this.detectCollisionWithBird(this.ground)){
+            this.node.y = this.ground.y + this.node.width / 2 +120 ;
+        }
+    },
 });
--- assets/script/Game.js
+++ assets/script/Game.js
@@ -1,103 +1,266 @@
 // Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
 // Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
 // Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
 var Bird = require('Bird');
-
+var PipeManager = require('PipeManager')
+var Scroller = require('Scroller')
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-        /** 遮罩节点 */
         maskLayer: {
-            default: null,
-            type: cc.Node
+            type: cc.Node,
+            default: null
         },
-        /** 准备开始菜单节点 */
-        readyMenu: {
-            default: null,
-            type: cc.Node
+        readyMenu:{
+            type: cc.Node,
+            default: null
         },
-        /** 小鸟组件 */
-        bird: Bird,
+        bird:Bird,
+        pipeManager:PipeManager,
+        //的金牌的分数
+        goldScore:3,
+        //得银牌的分数
+        silverScore:1,
+        //分数线式节点
+        scoreLabel:cc.Label,
+        //地面节点
+        ground:{
+            default:null,
+            type:cc.Node
+        },
+        //背景节点
+        background:{
+            default:null,
+            type:cc.Node
+        },
+        gameOverMenu:{
+            default:null,
+            type:cc.Node
+        },
+        //得分声音
+        scoreAudio:{
+            default:null,
+            url:cc.AudioClip
+        },
+
+        //按钮点击、节点浮现的声音
+        swooshingAudio:{
+            default:null,
+            url:cc.AudioClip
+        }
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad() {
+    // onLoad () {},
+    onLoad(){
+        this.score = 0;
+        this.scoreLabel.string = this.score;
         this._revealScene();
         this._enableInput(true);
         this.bird.init(this);
     },
-    _revealScene() {
+
+    start () {
+
+    },
+    _revealScene(){
         this.maskLayer.active = true;
         this.maskLayer.color = cc.Color.BLACK;
         this.maskLayer.runAction(cc.fadeOut(0.3));
     },
-    start() {
 
-    },
-    _enableInput(enable) {
-        if (enable) {
-            cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-            this.node.on(cc.Node.EventType.TOUCH_START, this.onTouch, this);
-            cc.log("1234567890")
-        } else {
-            cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-            this.node.off(cc.Node.EventType.TOUCH_START, this.onTouch, this);
-            cc.log("1qwertyuiop")
+    // update (dt) {},
+    _enableInput: function(enable){
+        if(enable){
+            cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN,this.onKeyDown,this);
+            this.node.on(cc.Node.EventType.TOUCH_START,this.onTouch,this);
+        }else{
+            cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN,this.onKeyDown,this);
+            this.node.off(cc.Node.EventType.TOUCH_START,this.onTouch,this);
         }
     },
-    onKeyDown() {
-        this._startGameOrJumpBird();
-    },
-
-    onTouch() {
-        this._startGameOrJumpBird();
-        return true;
-    },
-    _hideReadyMenu() {
+    _hideReadyMenu(){
         this.readyMenu.runAction(
             cc.sequence(
                 cc.fadeOut(0.5),
-                cc.callFunc(() => {
+                cc.callFunc(()=>{
                     this.readyMenu.active = false;
-                }, this)
+                },this)
             )
         );
     },
+    //定义键盘事件 处理函数，鼠标点击事件函数
+    onKeyDown(){
+        // this._hideReadyMenu();
+        this._startFGameOrJumpBird();
+    },
+    onTouch(){
+        // this._hideReadyMenu();
+        this._startFGameOrJumpBird(); 
+        return true;
+    },
+
+
     //开始或者飞行
-    _startGameOrJumpBird() {
-        if (this.bird.state === Bird.State.Ready) {
+    _startFGameOrJumpBird(){
+        //如果是准备状态则运行开始游戏，否则继续飞行
+        if(this.bird.state === Bird.State.Ready){
             this._gameStart();
-        } else {
-            this.bird.rise();//继续飞行
+        }else{
+            //继续飞行
+            this.bird.rise();
         }
     },
     //游戏开始
-    _gameStart() {
-        this._hideReadyMenu();//隐藏menu
-        this.bird.startFly();//开始飞行
+    _gameStart(){
+        //隐藏menu
+        this._hideReadyMenu();
+        //开始飞行
+        this.bird.startFly();
+        this.pipeManager.startSpawn();
     },
-    // update (dt) {},
+    gameOver(){
+        this.pipeManager.reset();
+        this._enableInput(false);
+
+        this.ground.getComponent(Scroller).stopScroll(); //地板停止滚动
+        this.background.getComponent(Scroller).stopScroll();//背景停止滚动
+        this.blinkOnce();//闪烁一次
+        this.showGameOverMenu();//显示有些结束菜单
+    },
+    //屏幕闪烁一下
+    blinkOnce() {
+        this.maskLayer.color = cc.Color.WHITE;
+        this.maskLayer.runAction(
+            cc.sequence(
+                cc.fadeTo(0.1, 200),
+                cc.fadeOut(0.1)
+            )
+        );
+    },
+    //隐藏分数
+    showGameOverMenu() {
+
+        this.scoreLabel.node.runAction(
+            cc.sequence(
+                cc.fadeOut(0.3),
+                cc.callFunc(()=>{
+                    this.scoreLabel.node.active = false;
+                },this)
+            )
+        );
+        //获取游戏结束界面的各个节点
+        let gameOverNode = this.gameOverMenu.getChildByName("gameOverLabel");
+        let resultBoardNode = this.gameOverMenu.getChildByName("resultBoard");
+        let startButtonNode = resultBoardNode.getChildByName("startButton");
+        let medalNode = resultBoardNode.getChildByName("medal");
+        let currentScoreNode = resultBoardNode.getChildByName("currentScore");
+        let bestScoreNode = resultBoardNode.getChildByName("bestScore");
+
+        //保存最高分到本地
+        const KEY_BEST_SCORE = "bestScore";
+        let bestScore = cc.sys.localStorage.getItem(KEY_BEST_SCORE);
+        if(bestScore === "null" || this.score>bestScore){
+            bestScore = this.score;
+        }
+        cc.sys.localStorage.setItem(KEY_BEST_SCORE,bestScore);
+
+        //显示当前分数、做高分
+        currentScoreNode.getComponent(cc.Label).string = this.score;
+        bestScoreNode.getComponent(cc.Label).string = bestScore;
+
+        //决定是否显示奖牌
+
+        // let showMedal = (err, spriteFrame) =>{
+        //     medalNode.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+        // };
+        // if(this.score >= this.goldScore){
+        //     //显示金牌
+        //     console.log("显示金牌")
+        //     cc.loader.loadRes("texture/medal_gold",cc.SpriteFrame,showMedal);
+        // }else if(this.score >= this.silverScore){
+        //     //显示银牌
+        //     console.log("显示银牌"+this.silverScore)
+        //     cc.loader.loadRes("texture/medal_silver",cc.SpriteFrame, showMedal);
+        // }else{
+        //     showMedal();
+        // }
+
+        let showMedal = (err, spriteFrame) => {
+            medalNode.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+            console.log("显示奖牌的方法调用")
+        };
+        console.log(this.score+"++++"+this.goldScore+"++++"+this.silverScore)
+            if (this.score >= this.goldScore) { // 显示金牌
+                console.log("金牌。。。。。。。。。")
+                cc.loader.loadRes("texture/medal_gold",cc.SpriteFrame, showMedal);
+            } else if (this.score >= this.silverScore) { // 显示银牌
+                console.log("银牌。。。。。。。。。")
+                cc.loader.loadRes("texture/medal_silver", cc.SpriteFrame,showMedal);
+            } 
+            else { // 不显示奖牌
+                showMedal();
+            }
+
+        //依次显示各个节点
+        var showNode = (node, action, callback) =>{
+            startButtonNode.active =true;
+            cc.audioEngine.playEffect(this.swooshingAudio)
+            node.runAction(cc.sequence(
+                action,
+                cc.callFunc(()=>{
+                    if(callback){
+                        callback();
+                    }
+                },this)
+            ));
+        };
+        this.gameOverMenu.active = true;
+        let showNodeFunc = () => showNode(
+            gameOverNode,
+            cc.spawn(
+                cc.fadeIn(0.2),
+                cc.sequence(
+                    cc.moveBy(0.2, cc.p(0, 10)),
+                    cc.moveBy(0.5, cc.p(0, -10)),
+                )
+            ),
+            ()=>showNode(
+                resultBoardNode,
+                cc.moveTo(0.5, cc.p(resultBoardNode.x, -250)).easing(cc.easeCubicActionOut()),
+                ()=>showNode(
+                    startButtonNode,
+                    cc.fadeIn(0.5)
+                )
+            )
+        );
+        this.scheduleOnce(showNodeFunc, 0.55);
+      
+    },
+    gainScore(){
+        this.score++;
+        this.scoreLabel.string = this.score;
+        cc.audioEngine.playEffect(this.scoreAudio)
+    },
+    restart(){
+        this.maskLayer.color = cc.Color.BLACK;
+        this.maskLayer.runAction(
+            cc.sequence(
+                cc.fadeIn(0.3),
+                cc.callFunc(()=>{
+                    //重新加载场景
+                    cc.director.loadScene('game');
+                },this)
+            )
+        );
+        cc.audioEngine.playEffect(this.swooshingAudio);
+    },
 });
